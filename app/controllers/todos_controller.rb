b73c5d450d84eddca6532285c6924fb5f1c19b16
effgenij <effgenij@gmail.com>
2020-12-17 01:05:08 +0300

    add todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,34 @@
 class TodosController < ApplicationController
+  before_action :set_project
+  before_action :set_todo, only: :update
+
   def create
-    render json: { message: 'create' }
+    @project.todos.create(todo_params)
   end
 
   def update
-    render json: { message: 'update' }
+    @todo.update(isCompleted: params[:isCompleted])
+  end
+
+  private
+
+  def set_project
+    @project = find_project || create_project
+  end
+
+  def find_project
+    Project.find(params[:project_id])
+  end
+
+  def create_project
+    Project.create(title: params[:project.title])
+  end
+
+  def set_todo
+    @todo = @project.todos.find(params[:id])
+  end
+
+  def todo_params
+    params.permit(:text, :isCompleted, :project_id)
   end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
 
-class TodosControllerTest < ActionDispatch::IntegrationTest
+class TodosControllerTest < ActionController::TestCase
   test "should get create" do
+    create :todo
     get todos_create_url
     assert_response :success
   end
 
   test "should get update" do
+    create :todo
     get todos_update_url
     assert_response :success
   end
